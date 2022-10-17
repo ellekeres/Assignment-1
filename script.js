025f120339d4fbb4e546190cdd5f63b19207d55c
ellekeres <113652352+ellekeres@users.noreply.github.com>
2022-10-17 02:53:12 +0000

    refining the form: password validation and username

---
+++ script.js
@@ -0,0 +1,37 @@
+function validatePassword() {
+  var password1 = document.getElementById("pword").value;
+  var password2 = document.getElementById("pword2").value;
+
+
+  //seeing if each password matches
+  if (password1 != password2) {
+    alert("validation failed false");
+    return false;
+  }
+  
+  return true;
+  
+}
+//making it to where these usernames cannot be used
+function validateUserName(){
+  var userNames = ["Harry", "Elizabeth", "Shana"]
+  var nameEntered = document.getElementById("user").value;
+
+  if (userNames.includes(nameEntered.toLowerCase())) {
+    alert("Name in use");
+    return false;
+  }
+  
+  return true;
+}
+
+
+function validateMyForm() {
+
+  if (validateUserName() && validatePassword()) {
+    return true;
+  }
+  
+  return false;
+}
+  
